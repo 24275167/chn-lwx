5b07aabcefeb1b964ee42a7ddc87dac1c3884028
24275167 <145706180+24275167@users.noreply.github.com>
2024-10-14 13:07:49 +0800

    Delete A3 directory

--- A3/api/crowdfunding_db.js
+++
@@ -1,9 +0,0 @@
-const { createConnection } = require('mysql2');
-// mysql连接
-const mysql = createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456',
-  database: 'crowdfunding_db',
-}).promise();
-module.exports = mysql
